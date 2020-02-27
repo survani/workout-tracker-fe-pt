6b82671344b967e9434ed5540407399c5e6a0750
Carl Padilla <carlpadilla@protonmail.com>
2020-02-27 09:45:17 -0500

    added in the select option for body part to work out

--- src/components/dashboard/Scheduler.js
+++ src/components/dashboard/Scheduler.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Time from './Time';
+import { Formik, Field, Form } from 'formik';
 
 const Scheduler = () => {
   return (
     <div>
-      <Time />
+      <Formik>
+        <Form>
+          <h2>Coming Up Today</h2>
+          <Field as='select' value='bodyparts'>
+            <option value='upperBody'>Upper Body</option>
+          </Field>
+          <Time />
+        </Form>
+      </Formik>
     </div>
   );
 };
