56ca5f65fc7bb657755c270accb8243e1ff95f4d
Paul Abernathy <paul.abernathy7@gmail.com>
2020-03-07 14:23:12 -0500

    fixed up app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./App.css";
 import { render } from "@testing-library/react";
+import Callback from "../src/components/Auth/Auth/Callback";
+import Signup from "../src/components/Auth/Auth/Auth";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Route exact path="/" component={Signup} exact />
+      <Route exact path="/callback" component={Callback} exact />
+    </div>
+  );
 }
 
 export default App;
