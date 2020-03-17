7ef241aa9f3d20355367eba13bf4c4ad1ef9a835
LiamColeE <liamedlinger@gmail.com>
2020-03-16 20:40:21 -0500

    moved all router things to the router.js

--- src/components/Router/Routes.js
+++ src/components/Router/Routes.js
@@ -4,6 +4,8 @@ import { Route } from 'react-router-dom';
 //components
 import Onboarding from '../onboarding/Onboard';
 import Dashboard from '../dashboard/Dashboard';
+import Callback from "../Auth/Auth/Callback";
+import Signup from "../Auth/Auth/Signup";
 
 
 // protected comopnents will be used with PrivateRoute. You can look at what the code is doing later.
