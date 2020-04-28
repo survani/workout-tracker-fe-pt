b3a31d909087831f70119e53baf7e4b8ae943141
Carl Padilla <carlpadilla@protonmail.com>
2020-04-28 09:49:59 -0400

    dummy data entered for calendar

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -9,12 +9,23 @@ import './calendar.css';
 const Calendar = () => {
   return (
     <div>
-      <div className="calendar_main">
-        <h1 className="calendar_headline">Schedule</h1>
-    </div>
-      
+      <div className='calendar_main'>
+        <h1 className='calendar_headline'>Schedule</h1>
+      </div>
+
       <div className='calendar-container'>
-        <FullCalendar defaultView='dayGridMonth' plugins={[dayGridPlugin]} />
+        <FullCalendar
+          defaultView='dayGridMonth'
+          plugins={[dayGridPlugin]}
+          events={[
+            { title: 'Biceps', date: '2020-04-28' },
+            { title: 'Legs', date: '2020-04-29' },
+            { title: 'Cardio', date: '2020-04-30' },
+            { title: 'Back', date: '2020-05-01' },
+            { title: 'Chest', date: '2020-05-04' },
+            { title: 'Arms', date: '2020-05-05' },
+          ]}
+        />
       </div>
     </div>
   );
