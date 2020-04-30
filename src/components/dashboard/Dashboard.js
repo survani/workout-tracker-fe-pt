4e29db2db5d92643abc2682889a080724dbd5f03
Jennifer Hott-Leitsch <jenniferhott-leitsch@Jennifers-Mini.hsd1.il.comcast.net>
2020-04-29 20:28:33 -0500

    updated dashboard button

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -2,15 +2,18 @@ import React from 'react';
 import Scheduler from './Scheduler';
 import Progress from './Progress';
 import "./dashboard.css";
+import { useHistory } from "react-router-dom";
 
 
 const Dashboard = () => {
+  const history = useHistory();
+
   return (
     <div>
       <div className="dashboard_main"><h1 className="dashboard_header">Dashboard</h1></div>
       <Scheduler />
       <Progress />
-      <button style={{ marginTop: '50px' }} type='submit' className="dashboard_btn">
+      <button style={{ marginTop: '50px' }} type='submit' className="dashboard_btn" onClick={() => history.push("/schedule")}>
         Schedule
       </button>
     </div>
