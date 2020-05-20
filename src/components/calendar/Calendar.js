7b64e7705f6618120b7b759aaf67bcb45b2e09c1
Kristi Gribble <kgribble4083@gmail.com>
2020-05-19 22:54:00 -0500

    Deleted css to install sass, removed css imports

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -4,7 +4,6 @@ import dayGridPlugin from '@fullcalendar/daygrid';
 
 import '@fullcalendar/core/main.css';
 import '@fullcalendar/daygrid/main.css';
-import './calendar.css';
 
 const Calendar = () => {
   return (
--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import "./signup.css"
 import { Form, Field, withFormik } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
