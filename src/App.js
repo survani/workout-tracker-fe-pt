cc84bb031f2438b09cc42aa3a90b28debc973b31
Survani <giovanicode@outlook.com>
2020-05-22 19:28:43 -0700

    fixed a few imports that had outdated paths...sinced I renamed.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 //components
-import Routes from './components/Router/Routes';
-import Navigation from './components/Navigation/Navigation';
+import Routes from './components/router/Routes';
+import Navigation from './components/navigation/Navigation';
 import Footer from './components/dashboard/Footer';
 
 function App() {
