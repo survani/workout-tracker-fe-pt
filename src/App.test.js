847e3ec5783848183b6413e777ba577a66bd4ba3
Nathan <nate1898@gmail.com>
2020-07-08 21:34:58 -0400

    Fix testing (#57)

---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+// jest-dom adds custom jest matchers for asserting on DOM nodes.
+// allows you to do things like:
+// expect(element).toHaveTextContent(/react/i)
+// learn more: https://github.com/testing-library/jest-dom
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
+import App from './App';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
