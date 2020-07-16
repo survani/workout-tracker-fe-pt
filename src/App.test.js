242365bcfc0e58b977d34f68e36806b22a9751ff
Kristi Gribble <kgribble4083@gmail.com>
2020-07-15 22:28:16 -0500

    Removed imports not needed

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,3 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
--- src/components/register/Signup.test.js
+++ src/components/register/Signup.test.js
@@ -1,19 +1,19 @@
 import React from "react";
-import ReactDOM from 'react-dom';
 import Signup from "./register";
-import {
-    BrowserRouter
-} from "react-router-dom";
-import '@testing-library/jest-dom/extend-expect';
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom/extend-expect";
 import * as rtl from "@testing-library/react";
-import App from '../../App';
+import MutationObserver from "mutation-observer";
 
-import MutationObserver from 'mutation-observer'
 global.MutationObserver = MutationObserver;
 
 test("Render the Sign Up text", () => {
-    const wrapper = rtl.render( <BrowserRouter> <Signup/> </BrowserRouter>);
+  const wrapper = rtl.render(
+    <BrowserRouter>
+      <Signup />
+    </BrowserRouter>
+  );
 
-    const expecttitle = wrapper.getAllByText(/Sign Up/i); 
-    expect(expecttitle.length).toEqual(2);
+  const expecttitle = wrapper.getAllByText(/Sign Up/i);
+  expect(expecttitle.length).toEqual(2);
 });
