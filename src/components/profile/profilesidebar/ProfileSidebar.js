24abb8cb87ba0b6f9ef4748bcd6059efb9ccbe34
Giovani Garfias <46204471+survani@users.noreply.github.com>
2020-07-26 21:28:43 -0700

    Cleaning The Codebase (#78)
    * cleaned up the logout button and fixed a bug in the logout
    
    * Finishing cleaning warnings and code.

--- src/components/profile/profilesidebar/ProfileSidebar.js
+++ src/components/profile/profilesidebar/ProfileSidebar.js
@@ -22,6 +22,7 @@ const ProfileSidebar = () => {
         <ProfileAside>
           <AsideTitle> Profile </AsideTitle>
           <img src={ProfilePic} alt="Profile Avatar" />
+          <UserName>{userInfo.username}</UserName>
           <UsernameContainer>
             <UserName>{userInfo.username}</UserName>
             <VerifiedUser />
