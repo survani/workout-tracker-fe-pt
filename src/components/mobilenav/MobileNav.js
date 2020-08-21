fe11df26b8501a3f58aa8f8143158e3843c99d49
Jessica Lam <jemlam93@gmail.com>
2020-08-21 10:07:38 -0700

    created new navigation menu

--- src/components/mobilenav/MobileNav.js
+++ src/components/mobilenav/MobileNav.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
 import Dashboard from "../../assets/mobilenav/dashboard.svg";
 import Food from "../../assets/mobilenav/food.svg";
@@ -12,108 +12,50 @@ import Logout from "../../assets/mobilenav/logout.svg";
 import { NavLink } from "react-router-dom";
 
 const MobileNav = () => {
-  const Signout = () => {
+  const Logout = () => {
     localStorage.clear();
   };
+
+  const select = () => {
+    var burger = document.querySelector('.burger-container'),
+        header = document.querySelector('.header');
+    
+    burger.onclick = function() {
+        header.classList.toggle('menu-opened');
+    }
+};
+
+useEffect(() => {
+  select();
+}, []);
   
   return (
-    <div class="container">
-    <section class="menu menu--circle">
-  <input type="checkbox" id="menu__active"/>
-  <label htmlFor="menu__active" class="menu__active">
-    <div class="menu__toggle">
-      <div class="icon">
-        <div class="hamburger"></div>
+    <div className="window">
+  <div className="header">
+    <div className="burger-container">
+      <div id="burger">
+        <div className="bar topBar"></div>
+        <div className="bar btmBar"></div>
       </div>
     </div>
-    <input type="radio" name="arrow--up" id="degree--up-0"/>
-    <input type="radio" name="arrow--up" id="degree--up-1" />
-    <input type="radio" name="arrow--up" id="degree--up-2" />
-    <div class="menu__listings">
-      <ul class="circle">
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-                <NavLink to="/dashboard"><img src={Dashboard} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/diets"><img src={Food} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-              <a href="#">&nbsp</a>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink onClick={Signout}to="/"><img src={Logout} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/createworkout"><img src={WorkoutForm} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/createnutrition"><img src={NutritionForm} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/calendar"><img src={Calendar} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/feed"><img src={Feed} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/workouts"><img src={Workout} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div class="placeholder">
-            <div class="upside">
-            <NavLink to="/profile"><img src={Profile} class="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div class="menu__arrow menu__arrow--top">
-      <ul>
-        <li>
-          <label htmlFor="degree--up-0"><div class="arrow"></div></label>
-          <label htmlFor="degree--up-1"><div class="arrow"></div></label>
-          <label htmlFor="degree--up-2"><div class="arrow"></div></label>
-        </li>
-      </ul>
-    </div>
-  </label>
-</section>
+    {/* <div className="icon icon-apple"></div> */}
+    <ul className="menu">
+      <li className="menu-item"><NavLink to="/workouts">Workouts</NavLink></li>
+      <li className="menu-item"><NavLink to="/dashboard">Dashboard</NavLink></li>
+      <li className="menu-item"><a href="#">iPhone</a></li>
+      <li className="menu-item"><a href="#">Watch</a></li>
+      <li className="menu-item"><a href="#">TV</a></li>
+      <li className="menu-item"><a href="#">Music</a></li>
+      <li className="menu-item"><a href="#">Support</a></li>
+      <li className="menu-item"><a href="#">Watch</a></li>
+      <li className="menu-item"><a href="#">TV</a></li>
+      <li className="menu-item"><a href="#">Music</a></li>
+      <li className="menu-item"><a href="#">Support</a></li>
+    </ul>
+    {/* <div className="shop icon icon-bag"></div>
+  </div>
+  <div className="content"> <img src="https://images.apple.com/v/ipad-air-2/c/images/overview/performance_large.png" alt=""/> */}
+</div>
 </div>
 
   );
