28ef15e1ad5c006a6bf9648da5fdb5e495eacaa8
Jessica Lam <55227810+jess-lam@users.noreply.github.com>
2020-08-23 20:26:01 -0700

    Feature/newmobilenav (#105)
    * created mobile navigation and got testing coverage to be 35.97%
    
    * imported movile navigation and styled the calendar, workoutform, and diet form
    
    * debugging
    
    * created random rng
    
    * created new navigation menu
    
    * deleted commented out code
    
    * hid mobile navigation for desktop
    
    * changed color of tthe mobile navigation menu
    
    Co-authored-by: Jessica Lam <jemlam@jessicas-mbp.lan>

--- src/components/mobilenav/MobileNav.js
+++ src/components/mobilenav/MobileNav.js
@@ -1,119 +1,48 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./style.css";
-import Dashboard from "../../assets/mobilenav/dashboard.svg";
-import Food from "../../assets/mobilenav/food.svg";
-import Workout from "../../assets/mobilenav/workout.svg";
-import Profile from "../../assets/mobilenav/profile.svg";
-import Feed from "../../assets/mobilenav/feed.svg";
-import Calendar from "../../assets/mobilenav/calendar.svg";
-import NutritionForm from "../../assets/mobilenav/nutritionform.svg";
-import WorkoutForm from "../../assets/mobilenav/workoutform.svg";
-import Logout from "../../assets/mobilenav/logout.svg";
 import { NavLink } from "react-router-dom";
 
 const MobileNav = () => {
-  const Signout = () => {
+  const Logout = () => {
     localStorage.clear();
   };
+
+  const select = () => {
+    var burger = document.querySelector('.burger-container'),
+        header = document.querySelector('.header');
+    
+    burger.onclick = function() {
+        header.classList.toggle('menu-opened');
+    }
+};
+
+useEffect(() => {
+  select();
+}, []);
   
   return (
-    <div className="container">
-    <section className="menu menu--circle">
-  <input type="checkbox" id="menu__active"/>
-  <label htmlFor="menu__active" className="menu__active">
-    <div className="menu__toggle">
-      <div className="icon">
-        <div className="hamburger"></div>
+    <div className="window">
+  <div className="header">
+    <div className="burger-container">
+      <div id="burger">
+        <div className="bar topBar"></div>
+        <div className="bar btmBar"></div>
       </div>
     </div>
-    <input type="radio" name="arrow--up" id="degree--up-0"/>
-    <input type="radio" name="arrow--up" id="degree--up-1" />
-    <input type="radio" name="arrow--up" id="degree--up-2" />
-    <div className="menu__listings">
-      <ul className="circle">
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-                <NavLink to="/dashboard"><img src={Dashboard} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/diets"><img src={Food} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-              <a href="#">&nbsp</a>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink onClick={Signout}to="/"><img src={Logout} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/createworkout"><img src={WorkoutForm} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/createnutrition"><img src={NutritionForm} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/calendar"><img src={Calendar} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/feed"><img src={Feed} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/workouts"><img src={Workout} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-        <li>
-          <div className="placeholder">
-            <div className="upside">
-            <NavLink to="/profile"><img src={Profile} className="button"/></NavLink>
-            </div>
-          </div>
-        </li>
-      </ul>
-    </div>
-    <div className="menu__arrow menu__arrow--top">
-      <ul>
-        <li>
-          <label htmlFor="degree--up-0"><div className="arrow"></div></label>
-          <label htmlFor="degree--up-1"><div className="arrow"></div></label>
-          <label htmlFor="degree--up-2"><div className="arrow"></div></label>
-        </li>
-      </ul>
-    </div>
-  </label>
-</section>
+    <ul className="menu">
+      <li className="menu-item"><NavLink to="/dashboard">Dashboard</NavLink></li>
+      <li className="menu-item"><NavLink to="/profile">Profile</NavLink></li>
+      <li className="menu-item"><NavLink to="/feed">Feed</NavLink></li>
+      <li className="menu-item"><NavLink to="/createworkout">Create a Workout</NavLink></li>
+      <li className="menu-item"><NavLink to="/createroutine">Create a Routine</NavLink></li>
+      <li className="menu-item"><NavLink to="/createnutrition">Create a Food Entry</NavLink></li>
+      <li className="menu-item"><NavLink to="/workouts">View Workouts</NavLink></li>
+      <li className="menu-item"><NavLink to="/routines">View Routines</NavLink></li>
+      <li className="menu-item"><NavLink to="/diets">View Food Diary</NavLink></li>
+      <li className="menu-item"><NavLink to="/calendar">Calendar</NavLink></li>
+      <li className="menu-item"><NavLink onClick={Logout} to="/">Logout</NavLink></li>
+    </ul>
+</div>
 </div>
 
   );
